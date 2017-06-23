643f111ad4a0c53a2c7efe5b8a97288624304bfc
Mathias Hannus <mathias.hannus@nfs.fi>
2017-06-23 08:51:20 +0300

    code cleanup

--- server/src/views/games.php
+++ server/src/views/games.php
@@ -1,7 +1,4 @@
 <?php
-
-//$db = new PDO('mysql:host=localhost;dbname=viikkarit;charset=utf8mb4', 'viikkarit', 'arvilind');
-
 $otteluDao = new OtteluDao();
 
 if(! (empty($kaukalo) || empty($kentta))) {
@@ -13,12 +10,3 @@ if(! (empty($kaukalo) || empty($kentta))) {
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 echo json_encode($ottelut);
-/*
-$string = file_get_contents(__DIR__."/../otteluohjelma.json");
-//$json_a = json_decode($string, true);
-$json_a = json_decode($string);
-
-echo ($string);
-
-//var_dump($json_a);
-*/
